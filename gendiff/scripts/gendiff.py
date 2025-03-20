ab206f282c68c9478ba5e7b3590c025666310090
malina-prog <dima.sidol16@google.com>
2025-03-20 22:41:10 +0300

    make start point

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,11 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser.add_argument('first_file', type=str,)
+    parser.add_argument('second-file', type=str,)
+
+    args = parser.parse_args()
+    
+if __name__ == "__main__":
+    main()
