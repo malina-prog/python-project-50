bd011ddf486dede446467875466a17b0789ce66e
malina-prog <dima.sidol16@google.com>
2025-03-22 00:08:17 +0300

    add new start options

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,6 +4,7 @@ def main():
     parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
     parser.add_argument('first_file', type=str,)
     parser.add_argument('second-file', type=str,)
+    parser.add_argument("-f", "--format",help="Set format of output.",default="stylish",)
 
     args = parser.parse_args()
     
